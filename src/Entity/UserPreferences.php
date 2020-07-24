183b787ebf2544033be88144ccd310c38632f8a3
gustavofrancoporto@toptal.com <github!Q2w3e4r>
2020-07-24 09:14:27 -0300

    Added user preferences entity and repository Added missing migration files

---
+++ src/Entity/UserPreferences.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserPreferencesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserPreferencesRepository::class)
+ */
+class UserPreferences
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=8)
+     */
+    private $locale;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+}
